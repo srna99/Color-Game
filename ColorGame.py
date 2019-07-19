5548d58077973de98d702bc385f89ded0e19f1fa
Serena Cheng <serena.ch1999@gmail.com>
2019-07-19 13:32:42 -0400

    Change color and text of word label and add entry field.

--- ColorGame.py
+++ ColorGame.py
@@ -3,22 +3,77 @@ import random
 
 
 class ColorGame:
-    _time_left = 30
+
+    colors = ["Red", "Yellow", "Orange", "Green", "Blue", "Purple", "Black", "Brown", "Pink", "Gray"]
+    time_left = 30
+    score = 0
 
     def __init__(self, window):
+
         window.title("Color Game")
         window.geometry("400x250")
 
-        self._instruction_label = tkinter.Label(window, text="Type the color of the word, not the text!",
-                                                font=("Arial", 14))
-        self._instruction_label.pack()
+        self.instruction_label = tkinter.Label(window, text="Type the color of the word, not the text!",
+            font=("Arial", 14))
+        self.instruction_label.pack(pady=10)
 
-        self._start_label = tkinter.Label(window, text="Press Enter to Start.", font=("Arial", 12))
-        self._start_label.pack()
+        self.start_label = tkinter.Label(window, text="Press Enter to Start.", font=("Arial", 12))
+        self.start_label.pack()
 
-        # self._time_label = tkinter.Label(window, text="Time Left: " + str(self._time_left), font=("Arial", 14))
-        # self._time_label.pack()
+        self.score_label = tkinter.Label(window, text="Score: 0", font=("Arial", 14))
 
+        self.time_label = tkinter.Label(window, text="Time Left: " + str(self.time_left),
+            font=("Arial", 14))
+
+        self.color_text_label = tkinter.Label(window, fg=self.colors[0], text="Color", font=("Times", 40))
+
+        self.textfield = tkinter.Entry(window, font=("Arial", 14))
+
+        window.bind("<Return>", self.play_game)
+
+    def play_game(self, event):
+
+        self.start_label.pack_forget()
+        self.score_label.pack()
+        self.time_label.pack()
+        self.color_text_label.pack()
+        self.textfield.pack(fill="x", padx=100)
+
+        self.textfield.focus_set()
+
+        # if self.time_label == 30:
+        #     self.countdown()
+
+        self.change_color()
+
+    def change_color(self):
+
+        random_color = self.colors[random.randint(0, len(self.colors)-1)]
+        random_color_text = self.colors[random.randint(0, len(self.colors)-1)]
+
+        self.color_text_label.config(fg=random_color, text=random_color_text)
+        self.color_text_label.pack(pady=13)
+
+        self.check_answer(random_color)
+
+    def check_answer(self, correct_color):
+
+        global score
+
+        if self.textfield.get().lower() == correct_color.lower():
+
+            self.score += 1
+            print(self.score)
+            self.score_label.config(text="Score: " + str(self.score))
+
+        self.textfield.delete(0, tkinter.END)
+
+    def countdown(self):
+
+        pass
+
+
+# -- Main Driver Code --
 
 new_window = tkinter.Tk()
 
